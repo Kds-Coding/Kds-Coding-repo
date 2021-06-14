fd5dd9f6dc721bf5b4194407a1a1d1cad357f5b5
Caint-dev <Caint-dev@users.noreply.github.com>
2021-06-14 22:51:51 +0200

    Ajout de texte dans les pages algo et home ( Mathias)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,82 +30,58 @@
     <!-- Prendre des variables scss et faire potentielement un component.-->
     <div class="container">
       <div class="row">
-        <article class="col-lg-4" id="">
-          <div class="card" style="width: 100%; height: 30em; z-index: 2">
-            <div class="fond" style="background-color: #a12c2c"></div>
-
-            <img
-              class="image-fluid"
-              src="../../public/img/home/Algorithm.png"
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <h1 class="text-center" style="color: #a12c2c">ALGORITHMIE</h1>
-              <p class="card-text text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <article class="col-lg-4">
+          <div id="AlgoDiv" class="fondDiv redDiv">
+            <div class="blockImg">
+              <img src="../../public/img/home/Algorithm.png" alt="" />
+            </div>
+            <div class="CourseBlock">
+              <h1 class="text-center">ALGORITHMIE</h1>
+              <p>
+                Dans ces cours, tu apprendras les bases de la programmation et
+                sa logique ! (recommandé pour les débutants)
               </p>
+
               <button
                 type="button"
                 id="seeMore"
                 @click="$router.push('/algo'), scrollToTop()"
-                style="background-color: #a12c2c"
               >
                 See more
               </button>
             </div>
           </div>
         </article>
-
-        <article class="col-lg-4" id="">
-          <div class="card" style="width: 100%; height: 30em; z-index: 2">
-            <div class="fond" style="background-color: #1e6995"></div>
-
-            <img
-              class="image-fluid"
-              src="../../public/img/home/DevWeb.png"
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <h1 class="text-center" style="color: #1e6995">
-                DEVELOPPEMENT WEB
-              </h1>
-              <p class="card-text text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
+        <article class="col-lg-4">
+          <div id="WebDevDiv" class="fondDiv blueDiv">
+            <div class="blockImg">
+              <img src="../../public/img/home/DevWeb.png" alt="" />
+            </div>
+            <div class="CourseBlock">
+              <h1 class="text-center">DEVELOPPEMENT WEB</h1>
+              <p>Durant ces cours, tu vas apprendre à programmer un site web</p>
               <button
                 type="button"
                 id="seeMore"
                 @click="$router.push('/web'), scrollToTop()"
-                style="background-color: #1e6995"
               >
                 See more
               </button>
             </div>
           </div>
         </article>
-
-        <article class="col-lg-4" id="">
-          <div class="card" style="width: 100%; height: 30em; z-index: 2">
-            <div class="fond" style="background-color: #178c3f"></div>
-
-            <img
-              class="image-fluid"
-              src="../../public/img/home/DevObj.png"
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <h1 class="text-center" style="color: #178c3f">DEV ORIENTE</h1>
-              <p class="card-text text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <article class="col-lg-4">
+          <div class="fondDiv greenDiv">
+            <div id="ObjDevDiv" class="blockImg">
+              <img src="../../public/img/home/DevObj.png" alt="" />
+            </div>
+            <div id="ObjDevDiv" class="CourseBlock">
+              <h1 class="text-center">DEV ORIENTE</h1>
+              <p>
+                Durant ces cours, tu vas apprendre à programmer plusieurs petits
+                programmes que tu pourras lancer sur ton ordinateur.
               </p>
-              <button
-                type="button"
-                id="seeMore"
-                @click="$router.push('/obj'), scrollToTop()"
-                style="background-color: #178c3f"
-              >
-                See more
-              </button>
+              <button type="button" id="seeMore">En voir plus</button>
             </div>
           </div>
         </article>
@@ -115,7 +91,7 @@
 </template>
 
 <script>
-import Discover from "@/components/common/Discover.vue";
+import Discover from "../components/common/Discover.vue";
 
 // @ is an alias to /src
 export default {
@@ -130,7 +106,132 @@ export default {
 };
 </script>
 <style lang="scss">
-/******************* HEADER STYLE************ */
+/* une variable couleur*/
+$colorAlgo: #a12c2c;
+$colorWeb: #1e6995;
+$colorObj: #178c3f;
+
+.head img {
+  width: 100vh;
+}
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+  width: 100%;
+}
+.fondDiv {
+  position: relative;
+  width: 100%;
+  height: 300px;
+}
+.fondDiv.greenDiv {
+  background-color: $colorObj;
+}
+.fondDiv.redDiv {
+  background-color: $colorAlgo;
+}
+.fondDiv.blueDiv {
+  background-color: $colorWeb;
+}
+.blockImg {
+  position: absolute;
+  width: 410px;
+  height: 409px;
+  margin: 20px;
+}
+.CourseBlock {
+  width: 410px;
+  height: 233px;
+  background: #ffffff;
+  top: 56%;
+  margin: 20px;
+  position: absolute;
+  transform: translateY(50%);
+}
+#AlgoDiv h1 {
+  color: $colorAlgo;
+}
+#ObjDevDiv h1 {
+  color: $colorObj;
+}
+#WebDevDiv h1 {
+  color: $colorWeb;
+}
+article h1 {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 2.1em;
+  line-height: 135.94%;
+  margin: 25px;
+
+  /* identical to box height, or 49px */
+  text-align: center;
+}
+.fondDiv p {
+  width: 409px;
+  height: 113px;
+  left: 972px;
+  top: 594px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 23.7355px;
+  line-height: 32px;
+  text-align: center;
+
+  color: #4691bb;
+}
+
+h2 {
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+}
+
+p {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 27px;
+}
+#homePage article {
+  height: 700px;
+}
+article img {
+  height: 70%;
+  width: 100%;
+}
+#seeMore {
+  position: absolute;
+  bottom: -30%;
+  right: 50%;
+
+  transform: translate(50%);
+  border: none;
+  border-radius: 4px;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+
+  #AlgoDiv #seeMore {
+    background-color: $colorAlgo;
+  }
+  #WebDevDiv #seeMore {
+    background-color: $colorWeb;
+  }
+  #ObjDevDiv #seeMore {
+    background-color: $colorObj;
+  }
+}
+
+header .container {
+  position: relative;
+  z-index: 2;
+}
 header {
   position: relative;
   height: 100vh;
@@ -168,82 +269,4 @@ header .welcome {
   text-shadow: 0px 8.5px 8.5px rgba(0, 0, 0, 0.5);
   z-index: 2;
 }
-/******************* END OF HEADER STYLE************ */
-
-/******************* IMAGE STYLE************ */
-.img-fluid {
-  max-width: 100%;
-  height: 100%;
-  width: 100%;
-}
-.head img {
-  width: 100vh;
-}
-
-.image-fluid {
-  max-width: 100%;
-  height: 50%;
-}
-/******************* END OF IMAGE STYLE************ */
-
-/******************* TEXT STYLE************ */
-
-article h1 {
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 2.1em;
-  line-height: 135.94%;
-  margin: 15px;
-  width: 100%;
-  /* identical to box height, or 49px */
-  text-align: center;
-}
-article p {
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 23.7355px;
-  line-height: 32px;
-  text-align: center;
-
-  color: #4691bb;
-}
-
-h2 {
-  font-family: "Roboto", sans-serif;
-  font-weight: 400;
-}
-/*******************END OF TEXT STYLE************ */
-
-/******************* CONTENT STYLE************ */
-
-#homePage article {
-  height: 700px;
-}
-.fond {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 50%;
-  z-index: -1;
-  margin: -15px;
-}
-
-#seeMore {
-  position: absolute;
-  bottom: -20%;
-  right: 50%;
-
-  transform: translate(50%);
-  border: none;
-  border-radius: 4px;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-}
-/******************* END OF CONTENT STYLE************ */
 </style>
--- src/views/algorithm/Algo.vue
+++ src/views/algorithm/Algo.vue
@@ -1,6 +1,6 @@
 <template>
   <AlgoHeader id="AlgoHeader">
-    <h2 id="HeaderSubTitle">Cours et exercices</h2>
+    <h2 id="algoSubTitle">Cours et exercices</h2>
     <Discover></Discover>
   </AlgoHeader>
   <div id="AlgoConteneur">
@@ -18,8 +18,8 @@
           Niveau : Facile
           <h1>cours 1</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore.
+            Ce cours va te permettre de savoir à quoi ça sert et en quoi est-ce utile l'algorithmie dans la programmation
+             et d'entrer doucement dans la logique d'un programme.
           </p>
         </div>
         <div class="play">
@@ -132,15 +132,15 @@
 </template>
 
 <script>
-import AlgoHeader from "@/components/common/AlgoHeader.vue";
-import Discover from "@/components/common/Discover.vue";
-import Footer from "@/components/common/Footer.vue";
-import GoldMedal from "@/assets/svg/icons/medal/gold-medal";
-import SilverMedal from "@/assets/svg/icons/medal/silver-medal";
-import Avatar1 from "@/assets/svg/icons/Avatar/Avatar1";
-import Banner from "@/assets/svg/Headers/bannerObj.vue";
-import Arrow from "@/assets/svg/icons/buttons/arrow.vue";
-import Play from "@/assets/svg/icons/buttons/play_circle_filled.vue";
+import AlgoHeader from "../components/common/AlgoHeader.vue";
+import Discover from "../components/common/Discover.vue";
+import Footer from "../components/common/Footer.vue";
+import GoldMedal from "../assets/svg/icons/medal/gold-medal";
+import SilverMedal from "../assets/svg/icons/medal/silver-medal";
+import Avatar1 from "../assets/svg/icons/Avatar/Avatar1";
+import Banner from "../assets/svg/Headers/bannerObj.vue";
+import Arrow from "../assets/svg/icons/buttons/arrow.vue";
+import Play from "../assets/svg/icons/buttons/play_circle_filled.vue";
 
 export default {
   components: {
