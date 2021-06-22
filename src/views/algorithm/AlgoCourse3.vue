17a727f51ab08e0139bd51762fd06024dce73c8f
Gekimon12 <76741554+Gekimon12@users.noreply.github.com>
2021-06-22 11:03:42 +0200

    Create AlgoCourse3.vue

---
+++ src/views/algorithm/AlgoCourse3.vue
@@ -0,0 +1,19 @@
+<template>
+  <AlgoHeader>
+    <h2 id="HeaderSubtitle">cours 2 - Les variables</h2>
+    <p class="HeaderTitleText>
+      Passons à présent au dexième chapitre de notre cours d'algorithmie qui concerne les variables,
+      un élément central dans tout algorithme digne de ce nom.
+    </p>
+  </Algoheader>
+ </template>
+ 
+<script>
+  import AlgoHeader from "@/components/common/AlgoHeader.vue";
+  
+  export default {
+    components: {
+      AlgoHeader,
+    },
+  };
+</script>
