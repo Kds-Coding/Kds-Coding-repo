37e877643fc7e7a8f67c4806b85680aa22bfac85
Kvpso270 <60976385+Kvpso270@users.noreply.github.com>
2021-07-02 15:57:30 +0200

    Add files via upload
    Dev obj header component

---
+++ src/components/common/devObjHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <Banner rectColor="#46BB6E" pathColor="#178C3F">
+      <algorithm_left></algorithm_left>
+      <coding ></coding> 
+      <div id="headerTextBox">
+        <h1 class="text-white text-center courseTitle">DEV OBJ</h1>
+        <slot></slot>
+      </div>
+    </Banner>
+  </header>
+</template>
+
+<script>
+import coding from "../../assets/svg/icons/dev ori/coding";
+import algorithm_left from "../../assets/svg/icons/Algo/algorithm_left";
+import Banner from "../../assets/svg/Headers/bannerObj.vue";
+
+export default {
+  components: {
+    coding,
+    algorithm_left,
+    Banner,
+  },
+};
+</script>
+
+<style>
+</style>
+
